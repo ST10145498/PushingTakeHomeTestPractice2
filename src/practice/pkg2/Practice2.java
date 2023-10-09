bf705f6394143d46fe4fb52b599860b73cf279a9
ST10145498 <ST10145498@vcconnect.edu.za>
2023-10-09 14:00:01 +0200

    InitialCommit

---
+++ src/practice/pkg2/Practice2.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practice.pkg2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author USER
+ */
+public class Practice2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+    System.out.print("hi ");
+    
+        System.out.print(" hi ");
+        
+        System.out.println("");
+        
+        JOptionPane.showMessageDialog(null,"Hacked");
+         JOptionPane.showMessageDialog(null,"Iphone 13");
+          JOptionPane.showMessageDialog(null,"termination line");
+           JOptionPane.showMessageDialog(null,"");
+            JOptionPane.showMessageDialog(null,"show message");
+             JOptionPane.showMessageDialog(null,"show message");
+        
+        
+    
+    }
+    
+}
